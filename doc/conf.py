7e087664edb0f688803ec11764bc43cd1c9a3fd0
Christian Herber <christian.herber@oss.nxp.com>
2023-09-06 10:39:22 +0200

    Adding OpenHW group logo to documentation

--- doc/conf.py
+++ doc/conf.py
@@ -111,7 +111,7 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 html_theme_options = {'style_nav_header_background': '#DDDDDD'}
-html_logo = '../images/openhw-landscape.svg'
+html_logo = 'images/openhw-landscape.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
